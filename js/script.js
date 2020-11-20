756f3ddb079fb91c1fe7a73e18fc351f93bf7e44
Aleksandr <registr99999@gmail.com>
2020-11-20 20:17:20 +0300

    Выполнение домашнего задания к уроку №2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 90000;
+let income = 'разработка приложения на JS';
+let addExpenses = 'Комуналка, Памперсы, Бензин, Одежда, Еда';
+let deposit = true;
+let mission = 5000000;
+let period = 6;
 
-alert('Вывод мадального окна командой alert');
+// alert('Вывод мадального окна командой alert');
 
 
-console.log('Вывод сообщения в консоль!');
+// console.log('Вывод сообщения в консоль!');
+
+
+//=========================================================================================================================================================================================
+// Домашнее задание №2
+
+// вывод типа данных переменных
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+// вычисляем длину строки
+console.log(addExpenses.length);
+
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев' + ' и цель заработать' + ' ' + mission + ' руб');
+
+// переводим строку к нижнему регистру
+console.log(addExpenses.toLowerCase().split(', '));
+
+// посчёт дневного бюджета
+let budgetDay = (money / 30);
+
+console.log(budgetDay + ' ' + 'руб');
+
