0b14c1733d63daf3eb7638a51a00f99ebde1f57a
Aleksandr <registr99999@gmail.com>
2020-11-23 23:50:22 +0300

    выполнение домашнего задания по уроку №3

--- js/script.js
+++ js/script.js
@@ -1,34 +1,82 @@
-let money = 90000;
+'use strict';
+// Получение элементов со страницы и объявление переменных
+
+let money = +prompt('Ваш месячный доход?');
 let income = 'разработка приложения на JS';
-let addExpenses = 'Комуналка, Памперсы, Бензин, Одежда, Еда';
-let deposit = true;
-let mission = 5000000;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let mission = 1000000;
 let period = 6;
 
-// alert('Вывод мадального окна командой alert');
+let expense01 = prompt('Введите обязательную статью расходов?');
+let expense02 = prompt('Введите обязательную статью расходов?');
+
+let amount01 = +prompt('Во сколько это обойдется?');
+let amount02 = +prompt('Во сколько это обойдется?');
+
+// посчёт бюджета на месяц
+let budgetMonth = money - (amount01 + amount01);
+
+// посчёт дневного бюджета
+let budgetDay = (budgetMonth / 30);
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------------
+// Обработчики событий
+
+
+
+//------------------------------------------------------------------------------------------------------------------------
+// ВЫВОД В КОНСОЛЬ
+
+// вывод типа данных переменных
+console.log(typeof money);
+
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(money);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('и цель заработать' + ' ' + mission + ' руб');
+// переводим строку к нижнему регистру
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('Бюджен на месяц:' + ' ' + budgetMonth + ' ' + 'руб');
+console.log('Цель будет достигнута за:' + ' ' + Math.ceil(mission / budgetMonth) + ' ' + 'месяцев');
+console.log(Math.floor(budgetDay) + ' ' + 'Бюджет на день');
 
 
 // console.log('Вывод сообщения в консоль!');
 
-
-//=========================================================================================================================================================================================
-// Домашнее задание №2
-
-// вывод типа данных переменных
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
 // вычисляем длину строки
-console.log(addExpenses.length);
+// console.log(addExpenses.length);
 
-console.log('Период равен' + ' ' + period + ' ' + 'месяцев' + ' и цель заработать' + ' ' + mission + ' руб');
 
-// переводим строку к нижнему регистру
-console.log(addExpenses.toLowerCase().split(', '));
 
-// посчёт дневного бюджета
-let budgetDay = (money / 30);
 
-console.log(budgetDay + ' ' + 'руб');
+//------------------------------------------------------------------------------------------------------------------------
+// Функциональность, циклы, и прочие методы
 
+switch (true) {
+    case budgetDay >= 1200:
+        console.log('У вас высокий уровень дохода');
+        break;
+    case budgetDay >= 600 && budgetDay < 1200:
+        console.log('У вас средний уровень дохода');
+        break;
+    case budgetDay < 600:
+        console.log('К сожалению у вас уровень дохода ниже среднего');
+        break;
+    case budgetDay < 0:
+        console.log('Что то пошло не так');
+        break;
+    default:
+        console.log('дефолтный вывод');
+        break;
+}
