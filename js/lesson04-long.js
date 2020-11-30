432772ed79f70b0c50175c503e23731ef26e3430
Aleksandr <registr99999@gmail.com>
2020-11-30 23:46:40 +0300

    выполнение усложненной домашки по уроку №4

--- js/lesson04-long.js
+++ js/lesson04-long.js
@@ -1,6 +1,22 @@
-let text = prompt('Введите название любимого сериала');
-let fooText = function inputText(arg) {
-    if (text.length > 30) {
+function checkNaN (a) {
+    let res = prompt(a);
+
+    if (isNaN(res) || res ==='' || res === null) {
+        if (res.length < 30) {
+            console.log(res.trim());
+            console.log(res.length);
+        }else {
+            console.log(res.trim().substring(0, 30) + '...');
+            console.log(typeof res);
+            console.log(res.length);
+        }
         
     }
+    return res;
 }
+
+let text = checkNaN('введите название любимого фильма');
+
+
+
+
