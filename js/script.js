27f2dc1916b5122a5acf447c544ddf68b4e893b3
Aleksandr <registr99999@gmail.com>
2020-12-21 18:43:30 +0300

    Выполнение домашенго задания по уроку 23

--- js/script.js
+++ js/script.js
@@ -275,5 +275,35 @@ window.addEventListener('DOMContentLoaded', () => { // DOMContentLoaded он д�
     startSlide(1500)
   };
 
+  // -------------------------------------------   img   ------------------------------------------------
+  const img = () => {
+    const img = document.querySelectorAll('.command__photo');
+    img.forEach((item) => {
+      let src = item.getAttribute('src', 'value');
+      let data = item.dataset.img;
+      
+      item.addEventListener('mouseenter', () => {
+        item.setAttribute('src', data);
+      })
+      item.addEventListener('mouseleave', () => {
+        item.setAttribute('src', src);
+      })
+    });
+    
+  }
+  img();
+  // -------------------------------------------   validInput   ------------------------------------------------
+  const input = () => {
+    const input = document.querySelectorAll('input.calc-item');
+    input.forEach((item) => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/\D/g, '');
+      })
+      
+    })
+    
+  }
+
+  input();
   slider();
 });
